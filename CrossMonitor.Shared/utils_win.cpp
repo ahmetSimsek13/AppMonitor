925e472eda2ae23a9ed81981594ea71a26414648
eahmsim <ahmet.simsek@ericsson.com>
2018-07-31 16:43:27 +0300

    First Commit

---
+++ CrossMonitor.Shared/utils_win.cpp
@@ -0,0 +1,11 @@
+#include "utils.hpp"
+
+using namespace std;
+
+namespace performance {
+namespace monitor {
+namespace utils {
+
+} //namespace utils
+} //namespace monitor
+} //namespace performance
